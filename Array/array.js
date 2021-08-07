6702456998fa4fd0f23e64ad6ac0bbef45be8097
Dieg Luna <diegofcolunalopez@gmail.com>
2021-08-07 18:59:17 -0500

    Eliminando elementos del Array

--- Array/array.js
+++ Array/array.js
@@ -11,13 +11,37 @@ class MyArray {
     return this.data[index];
   }
 
+  // agregamos un item
   push(item) {
     this.data[this.length] = item;
     this.length++;
     return this.data;
   }
+
+  // borramos el ultimo elemento de un array
+  pop() {
+    const lastItem = this.data[this.length - 1];
+
+    delete this.data[this.length - 1];
+
+    this.length--;
+
+    return lastItem;
+  }
+
+  delete(index) {
+    const item = this.data[index];
+    this.shiftIndex(index);
+    return item;
+  }
+
+  shiftIndex(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length - 1;
+  }
 }
-
-
 
 const myArray = new MyArray();
