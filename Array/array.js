fcf80f027696fd8dc63d9d2b9572232dd858f667
Dieg Luna <diegofcolunalopez@gmail.com>
2021-08-07 07:26:56 -0500

    Construyendo nuestro Array

---
+++ Array/array.js
@@ -0,0 +1,23 @@
+// la manera normal de crear un array;
+// const array = ["Diego", "Leo", "Hazel"];
+
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  get(index) {
+    return this.data[index];
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.data;
+  }
+}
+
+
+
+const myArray = new MyArray();
