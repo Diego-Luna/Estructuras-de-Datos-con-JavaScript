17039d108d4e7e2cdcab60738932d3cae4eee456
Dieg Luna <diegofcolunalopez@gmail.com>
2021-08-07 19:00:34 -0500

    agregar un elemento al incio del array

--- Array/array.js
+++ Array/array.js
@@ -42,6 +42,16 @@ class MyArray {
     delete this.data[this.length - 1];
     this.length - 1;
   }
+
+  // Agrega un elemento al inicio del array
+  unshift(item) {
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1];
+    }
+    this.data[0] = item;
+    this.length++;
+    return this.data;
+  }
 }
 
 const myArray = new MyArray();
