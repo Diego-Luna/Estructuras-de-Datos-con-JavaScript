9224ebbbf5057008a7373f4aab687d7cc5b9f385
Dieg Luna <diegofcolunalopez@gmail.com>
2021-08-08 07:44:15 -0500

    Implementando el método Get, Remove, getAllKeys

--- hashTable/hashTable.js
+++ hashTable/hashTable.js
@@ -2,6 +2,7 @@ class HashTable {
   constructor(size) {
     this.data = new Array(size);
   }
+  // nuestra funcions generadora de hash
   hashMethod(key) {
     let hash = 0;
     for (let i = 0; i < key.length; i++) {
@@ -9,6 +10,8 @@ class HashTable {
     }
     return hash;
   }
+
+  // Agregamos un nuevo valor, en una posicion aleatoria
   set(key, value) {
     const address = this.hashMethod(key);
     if (!this.data[address]) {
@@ -16,6 +19,50 @@ class HashTable {
     }
     this.data[address].push([key, value]);
     return this.data;
+  }
+
+  // El metodo que nos devuelve el valor de nuestro string
+  get(key) {
+    const adrees = this.hashMethod(key);
+    const currentBucket = this.data[adrees];
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+
+  // metodo delete
+  remove(key) {
+    const address = this.hashMethod(key);
+    const currentBucket = this.data[address];
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          const element = currentBucket[i][1];
+          delete currentBucket[i][1];
+          return element;
+        }
+      }
+    }
+    return undefined;
+  }
+
+  // metodo para obtener todos los keys
+
+  getAllKeys() {
+    const keys = [];
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        for (let j = 0; j < this.data[i].length; j++) {
+          keys.push(this.data[i][j][0]);
+        }
+      }
+    }
+    return keys;
   }
 }
 
