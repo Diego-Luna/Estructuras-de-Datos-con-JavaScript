4646b014a41d9f88d60c69e1da21b09ca911cd8a
Dieg Luna <diegofcolunalopez@gmail.com>
2021-08-08 08:28:20 -0500

    Agregar nodos a la lista

--- linkedList/singly.js
+++ linkedList/singly.js
@@ -1,22 +1,24 @@
 // ejemplo
 // 1 --> 2 --> 3 --> 4 --> 5 --> null
 
-let singlyLinkedList = {
-  head: {
-    value: 1,
-    next: {
-      value: 2,
-      next: {
-        value: 3,
-        next: {
-          value: 4,
-          next: null,
-        },
-      },
-    },
-  },
-};
+// ejemplo en un objeto
+// let singlyLinkedList = {
+//   head: {
+//     value: 1,
+//     next: {
+//       value: 2,
+//       next: {
+//         value: 3,
+//         next: {
+//           value: 4,
+//           next: null,
+//         },
+//       },
+//     },
+//   },
+// };
 
+// la case de nuestros nodos
 class Node {
   constructor(value) {
     this.value = value;
@@ -35,6 +37,45 @@ class MySinglyLinkedList {
     // tenemos un conteo de los elemtos que contiene
     this.length = 1;
   }
+
+  // nuestro metodo para agregar elemtos al final.
+  append(value) {
+    const newNode = new Node(value);
+
+    // agregamos un nuevo nodo
+    this.tail.next = newNode;
+
+    // ponemos el nuevo nodo en la cola
+    this.tail = newNode;
+
+    this.length++;
+
+    return this;
+  }
+
+  // agregamos un elemetno en la cabeza
+  prepend(value) {
+    const newNode = new Node(value);
+
+    // agregamos el node al a cabeza
+    newNode.next = this.head;
+
+    // lo colocamos en la lista
+    this.head = newNode;
+
+    this.length++;
+
+    return this;
+  }
 }
 
 let mySinglyLinkedList = new MySinglyLinkedList(1);
+
+// prueva , con runner o en el navegador
+
+mySinglyLinkedList;
+
+mySinglyLinkedList.append(2);
+mySinglyLinkedList.append(3);
+
+mySinglyLinkedList.prepend(0);
