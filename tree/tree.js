7845b27feb3dcd0bf96a1b53cdfe50b524c54238
Dieg Luna <diegofcolunalopez@gmail.com>
2021-08-09 09:43:20 -0500

    Binary Search Tree

--- tree/tree.js
+++ tree/tree.js
@@ -40,6 +40,7 @@ class BinarySearchTree {
 
   search(value) {
     let currentNode = this.root;
+
     while (currentNode && currentNode.value != value) {
       if (value < currentNode.value) {
         currentNode = currentNode.left;
@@ -47,6 +48,7 @@ class BinarySearchTree {
         currentNode = currentNode.right;
       }
     }
+    
     if (!currentNode) return false;
     return currentNode;
   }
